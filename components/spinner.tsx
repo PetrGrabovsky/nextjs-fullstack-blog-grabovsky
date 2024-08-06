b7be25dc75eef880189f9503a436eb69fd0af5e1
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-08-06 21:21:39 +0200

    feat: Přidána komponenta Spinner. - Vytvořen soubor spinner.tsx. - Implementována komponenta Spinner pomocí ProgressBar z balíčku react-loader-spinner.

---
+++ components/spinner.tsx
@@ -0,0 +1,19 @@
+'use client';
+
+import { FC } from 'react';
+import { ProgressBar } from 'react-loader-spinner';
+
+// Funkční komponenta Spinner
+const Spinner: FC = () => {
+  return (
+    <ProgressBar
+      height={120} // Výška spinneru
+      width={120} // Šířka spinneru
+      borderColor="#000000" // Barva rámečku
+      barColor="#FFFFFF" // Barva proužku
+      ariaLabel="Common Loader" // Popis pro čtečky obrazovek
+    />
+  );
+};
+
+export default Spinner;
