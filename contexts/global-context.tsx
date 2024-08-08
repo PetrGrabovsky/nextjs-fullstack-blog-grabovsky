7a7932a60b77070ed59ac1531549dafe40051d63
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-08-08 22:47:20 +0200

    feat: Přidán Spinner při načítání session do GlobalState. - Přidána komponenta Spinner, která se zobrazí, když session není načtena. - Pridán hook useSession, pro získání dat o session.

--- contexts/global-context.tsx
+++ contexts/global-context.tsx
@@ -2,29 +2,34 @@
 
 import { createContext, FC, ReactNode, useState } from 'react';
 import { GlobalContextType } from '@/utils/types';
+import { useSession } from 'next-auth/react';
+import Spinner from '@/components/spinner';
 
 // Výchozí stav kontextu
 const initialState: GlobalContextType = {
-  // Inicializace stavu načítání jako false
-  loading: false,
-  // Výchozí prázdná funkce pro setLoading
-  setLoading: () => {},
+  loading: false, // Inicializace stavu načítání jako false
+  setLoading: () => {}, //Výchozí prázdná funkce pro setLoading
 };
 
 // Vytvoření kontextu GlobalContext s výchozím stavem
 export const GlobalContext = createContext<GlobalContextType>(initialState);
 
 interface GlobalStateProps {
-  // Potomci komponenty, které budou obaleny poskytovatelem kontextu
-  children: ReactNode;
+  children: ReactNode; // Potomci komponenty, které budou obaleny poskytovatelem kontextu
 }
 
 // Komponenta GlobalState poskytuje GlobalContext svým potomkům
 const GlobalState: FC<GlobalStateProps> = ({ children }) => {
   // Stav a setter pro načítání
   const [loading, setLoading] = useState<boolean>(false);
+  // Použití useSession hooku pro získání dat o session
+  const { data: session } = useSession();
+
+  // Pokud není session ještě načtena, zobrazí se Spinner komponenta
+  if (session === undefined) return <Spinner />;
 
   return (
+    // Poskytuje hodnoty a funkce všem potomkům
     <GlobalContext.Provider value={{ loading, setLoading }}>{children}</GlobalContext.Provider>
   );
 };
