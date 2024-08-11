8b83a359493816cd89558aeb8dc70e6e3721a656
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-08-11 21:28:44 +0200

    feat: Přidán seznam kategoríí categories a struktura formulářových prvků formControls. - Přidán seznam categories obsahující dostupné kategorie pro blogové příspěvku do souboru data-mappings.ts. - Přidán seznam formControls, obsahující definici jednotlivých formulářových polí pro vytváření nových příspěvků do data-mapping.ts.

--- utils/data-mappings.ts
+++ utils/data-mappings.ts
@@ -1,4 +1,4 @@
-import { MenuItem } from './types';
+import { CategoryOption, FormControlItem, MenuItem } from './types';
 
 // Pole objektů pro položky menu
 export const menuItems: MenuItem[] = [
@@ -21,5 +21,57 @@ export const menuItems: MenuItem[] = [
     id: 'search',
     label: 'Search',
     path: '/search', // Adresa search stránky
+  },
+];
+
+// Seznam dostupných kategorií pro blogové příspěvky
+export const categories: CategoryOption[] = [
+  {
+    value: 'application',
+    label: 'Application',
+  },
+  {
+    value: 'data',
+    label: 'Data',
+  },
+  {
+    value: 'software',
+    label: 'Software',
+  },
+  {
+    value: 'technology',
+    label: 'Technology',
+  },
+  {
+    value: 'science',
+    label: 'Science',
+  },
+];
+
+// Definice formulářových polí pro vytváření blogových příspěvků
+export const formControls: FormControlItem[] = [
+  {
+    id: 'title',
+    label: 'Title',
+    placeholder: 'Enter Blog Title',
+    type: 'text',
+    component: 'input',
+    options: [],
+  },
+  {
+    id: 'description',
+    label: 'Description',
+    placeholder: 'Enter Blog Description',
+    type: 'text',
+    component: 'textarea',
+    options: [],
+  },
+  {
+    id: 'category',
+    label: 'Category',
+    placeholder: 'Choose Blog Category',
+    type: '',
+    component: 'select',
+    options: categories,
   },
 ];
