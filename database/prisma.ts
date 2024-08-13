72d68b3835b0827486f9f1241477755440cf1750
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-08-13 22:46:26 +0200

    fix: Opraven nesprávný import PrismaClient v souboru prisma.ts. - Změněn import z @prisma/client/extension na správný @prisma/client.

--- database/prisma.ts
+++ database/prisma.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from '@prisma/client/extension';
+import { PrismaClient } from '@prisma/client';
 
 // Definice globální proměnné pro ukládání instance PrismaClient
 const globalPrisma = global as unknown as { prisma: PrismaClient };
