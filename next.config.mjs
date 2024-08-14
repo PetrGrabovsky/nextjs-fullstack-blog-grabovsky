30677b48b2a333954f2ce35b9db03943b2be9517
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-08-14 23:03:54 +0200

    chore: Přidána konfigurace pro povolení vzdálených zdrojů obrázků v Next.js. - Aktualizován soubor next.config.mjs povolením načítání obrázků z firebasestorage.googleapis.com a avatars.githubusercontent.com. - Ke konfiguraci přidány komentáře pro zajištění přehlednosti.

--- next.config.mjs
+++ next.config.mjs
@@ -1,4 +1,22 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
+
+// Konfigurace pro Next.js aplikaci
+const nextConfig = {
+  images: {
+    // Definice povolených vzdálených zdrojů obrázků
+    remotePatterns: [
+      {
+        // Povolení obrázků z Firebase Storage
+        protocol: 'https',
+        hostname: 'firebasestorage.googleapis.com',
+      },
+      {
+        // Povolení obrázků z GitHub Avatars
+        protocol: 'https',
+        hostname: 'avatars.githubusercontent.com',
+      },
+    ],
+  },
+};
 
 export default nextConfig;
