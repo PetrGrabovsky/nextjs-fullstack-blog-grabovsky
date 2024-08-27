febea9c177cef19ad55360cb5036b6196340ef84
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-08-27 10:51:31 +0200

    feat: Přidány klientské validace do formuláře v komponentě Create. - Přidána validace povinných polí (obrázek, titulek, popis, kategorie). - Přidána logika pro správu chybových zpráv v případě neplatných vstupů. - Implementována zpětná vazba pro uživatele na neplatné vstupy.

--- app/create/page.tsx
+++ app/create/page.tsx
@@ -14,68 +14,112 @@ import { initialBlogFormData } from '@/utils/initial-data';
 import { handleImageSaveToFirebase } from '@/utils/helpers';
 
 /**
- * Funkční komponenta Create umožňující uživatelům vytvořit nový blogový příspěvek
- * Zajišťuje formulářové rozhraní, kde uživatelé mohou nahrávat obrázky a vyplňovat informace
- * o blogovém příspěvku, které budou následně odeslány na server
+ * Funkční komponenta Create představuje formulář pro vytvoření nového blogového příspěvku.
+ * Obsahuje jak vstupní pole pro text, tak i možnost nahrát obrázek.
+ * Komponenta provádí validaci vstupních dat a následně je odesílá na server.
  */
 const Create: FC = () => {
-  // Přístup ke globálnímu stavu pomocí Context API
+  // Použití kontextu pro globální stav formulářových dat
   const { formData, setFormData } = useContext(GlobalContext);
-  // Lokální stav pro sledování, zda se obrázek nahrává do Firebase Storage
+  // Lokální stav pro sledování nahrávání obrázku do Firebase Storage
   const [imageLoading, setImageLoading] = useState<boolean>(false);
-  // Použití hooku useSession pro získání dat o uživateli z aktuální session
+  // Použití hooku useSession pro získání dat o aktuálním uživateli
   const { data: session } = useSession();
+  // Hook pro navigaci uživatele po úspěšném odeslání formuláře
   const router = useRouter();
+  // Stav pro uložení chybových zpráv jednotlivých vstupů formuláře
+  const [errors, setErrors] = useState<Partial<BlogFormData>>({});
 
   /**
-   * Funkce pro zpracování změny obrázku blogu
-   * Tato funkce je spuštěna při výběru obrázku uživatelem a následně tento obrázek nahraje
-   * na Firebase
+   * Funkce validateForm provádí validaci formulářových dat před jejich odesláním na server.
+   * Kontroluje, zda jsou všechna formulářová pole vyplněná a splňují požadované podmínky.
+   * Pokud je formulář validní vrací true jinak false.
+   */
+  const validateForm = (): boolean => {
+    // Inicializace objektu pro uložení chybových zpráv.
+    const newErrors: Partial<BlogFormData> = {};
+
+    // Validace povinného pole pro obrázek
+    if (!formData.image) {
+      newErrors.image = 'Image is required.';
+    }
+
+    // Validace povinného pole pro titulek
+    if (!formData.title || formData.title.trim().length < 3) {
+      newErrors.title = 'Title is required and must be at least 3 characters long.';
+    }
+
+    // Validace povinného pole pro popis (obsah)
+    if (!formData.description || formData.description.trim().length < 50) {
+      newErrors.description = 'Description is required and must be at least 50 characters long.';
+    }
+
+    // Validace povinného pole pro výběř kategorie
+    if (!formData.category) {
+      newErrors.category = 'Category is required.';
+    }
+
+    // Aktualizace stavu s chybami
+    setErrors(newErrors);
+
+    // Vrací true pokud neexistují žádné chyby jinak false
+    return Object.keys(newErrors).length === 0;
+  };
+
+  /**
+   * Funkce handleBlogImageChange zpracovává změnu souboru obrázku v poli formuláře.
+   * Nahraje vybraný obrázek do Firebase Storage a uloží URL do stavu.
    */
   const handleBlogImageChange = async (event: ChangeEvent<HTMLInputElement>) => {
-    if (!event.target.files) return; // Pokud nejsou vybrány žádné soubory, funkce se ukončí
-
+    if (!event.target.files) return; // Pokud není vybrán obrázek, funkce se ukončí
     setImageLoading(true); // Nastaví stav nahrávání na true, což zobrazí spinner
 
     try {
       // Uloží obrázek do Firebase a získá jeho URL
       const imageUrl = await handleImageSaveToFirebase(event.target.files[0]);
+      // Aktualizace globálního stavu s URL obrázku
       setFormData((prevData) => ({
         ...prevData,
-        image: imageUrl, // Aktualizuje URL obrázku v globálním stavu
+        image: imageUrl,
       }));
+      // Odstranění případné chybové zprávy pro pole obrázku
+      setErrors((prevErrors) => ({ ...prevErrors, image: undefined }));
     } catch (error) {
-      console.error('Error uploading image: ', error); // Logování případných chyb
+      // Logování chyby při nahrávání obrázku do konzole
+      console.error('Error uploading image: ', error);
     } finally {
       setImageLoading(false); // Nastaví stav nahrávání na false, což skryje spinner
     }
   };
 
   /**
-   * Funkce pro zpracování změn ve formulářových polích
-   * Tato funkce je universální pro všechna vstupní pole formuláře a aktualizuje globální stav
-   * při každé změně vstupu
+   * Funkce handleInputChange zpracovává změny ve vstupních polích formuláře.
+   * Dynamicky aktualizuje stav na základě názvu pole a hodnoty.
    */
   const handleInputChange = (
     event: ChangeEvent<HTMLInputElement | HTMLTextAreaElement | HTMLSelectElement>
   ) => {
-    // Destrukturalizace eventu pro získání názvu a hodnoty vstupu
     const { name, value } = event.target;
+    // Aktualizace stavu formulářových dat
     setFormData((prevData) => ({
       ...prevData,
-      [name]: value, // Dynamická aktualizace příslušného pole ve formData podle jeho názvu
+      [name]: value,
     }));
+    // Odstraní případné chybové zprávy pro danné pole
+    setErrors((prevErrors) => ({ ...prevErrors, [name]: undefined }));
   };
 
   /**
-   * Funkce pro uložení nového blogového příspěvku.
-   * Tato funkce odešle data z formuláře (včetně URL obrázku) na server pomocí HTTP POST
-   * požadavku, kde jsou data uložena do databáze. Úspěšnost operace je zatím logována.
-   * Po úspěšném uložení příspěvku se formulář restartuje na výchozí hodnoty, aby byl
-   * připravený k vytvoření nového příspěvku.
+   * Funkce handleSaveBlogPost odesílá formulářová data na server pro uložení nového příspěvku.
+   * Nejprve validuje data a poté provádí HTTP POST požadavek na server.
    */
   const handleSaveBlogPost = async () => {
-    // Odeslání dat na server pro uložení do databáze
+    // Validace formuláře před odesláním
+    if (!validateForm()) {
+      return; // Pokud je validace neúspěšná, funkce se ukončí
+    }
+
+    // Odeslání dat na server pomocí HTTP POST požadavku
     const res = await fetch('/api/blog-post/add-post', {
       method: 'POST',
       headers: {
@@ -91,8 +135,7 @@ const Create: FC = () => {
 
     const data = await res.json(); // Parsování odpovědi ze serveru
 
-    console.log(data, 'data'); // Debugging: loguje odpověď serveru
-
+    // Pokud je odeslání úspěšné, resetuje formulář a přesměruje uživatele na stránku s blogy
     if (data && data.success) {
       setFormData(initialBlogFormData); // Reset formuláře na výchozí hodnoty
       router.push('/blogs'); // Přesměrování na stránku s blogy
@@ -104,7 +147,6 @@ const Create: FC = () => {
       <div className="container">
         <div className="-mx-4 flex flex-wrap">
           <div className="w-full px-4">
-            {/* Formulářová část pro vytvoření nového blogu */}
             <div
               className={clsx(
                 'mb-12 rounded-md bg-primary/[3%] px-8 py-10 dark:bg-dark sm:p-[55px] lg:mb-5',
@@ -122,13 +164,12 @@ const Create: FC = () => {
               </h2>
               <div>
                 <div className="flex flex-col gap-3">
+                  {/* Sekce pro výběr a zobrazení obrázku */}
                   <div className="flex gap-3">
                     <div className={clsx(imageLoading ? 'w-1/2' : 'w-full')}>
-                      {/* Popis pro pole výběru obrázku */}
                       <label htmlFor="fileInput" className={labelClasses}>
                         Upload Blog Image
                       </label>
-                      {/* Input pro výběr obrázku */}
                       <input
                         id="fileInput"
                         accept="image/*"
@@ -137,8 +178,10 @@ const Create: FC = () => {
                         type="file"
                         className={clsx(inputClasses, 'mb-8')}
                       />
+                      {/* Zobrazení chybové zprávy pro obrázek, pokud existuje */}
+                      {errors.image && <p className="text-red-500">{errors.image}</p>}
                     </div>
-                    {/* Spinner pro indikaci nahrávání obrázku */}
+                    {/* Zobrazení spinneru při nahrávání obrázku */}
                     {imageLoading && (
                       <div className="w-1/2">
                         <Spinner />
@@ -146,14 +189,12 @@ const Create: FC = () => {
                     )}
                   </div>
                   <div className="-mx-4 flex flex-wrap">
-                    {/* Generování formulářových polí na základě definice v data-mappings.ts */}
+                    {/* Dynamické generování vstupních polí na základě formControls */}
                     {formControls.map((control) => (
                       <div key={control.id} className="w-full px-4">
-                        {/* Popis pro vstupní pole */}
                         <label htmlFor={control.id} className={labelClasses}>
                           {control.label}
                         </label>
-                        {/* Vstupní pole podle definice v data-mappings.ts */}
                         {control.component === 'input' && (
                           <input
                             id={control.id}
@@ -197,6 +238,10 @@ const Create: FC = () => {
                               </option>
                             ))}
                           </select>
+                        )}
+                        {/* Zobrazení chybové zprávy pro pole, pokud existuje */}
+                        {errors[control.id as keyof BlogFormData] && (
+                          <p className="text-red-500">{errors[control.id as keyof BlogFormData]}</p>
                         )}
                       </div>
                     ))}
