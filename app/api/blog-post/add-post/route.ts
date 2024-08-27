c1b24c396d890ff944f1520f26da35fcca24ea40
Petr Grabovsky <petrgrabovsky@gmail.com>
2024-08-27 10:58:13 +0200

    feat: Přidány serverové validace v routě add-post pro zajištění integrity dat.

--- app/api/blog-post/add-post/route.ts
+++ app/api/blog-post/add-post/route.ts
@@ -4,12 +4,41 @@ import { NextRequest, NextResponse } from 'next/server';
 /**
  * Handler pro HTTP POST požadavek na vytvoření nového blogového příspěvku.
  * Zpracovává příchozí požadavek, extrahuje data blogového příspěvku z těla požadavku,
- * a následně tato data uloží do databáze pomocí Prisma klienta.
+ * provádí validaci dat a následně tato data uloží do databáze pomocí Prisma klienta.
  */
 export const POST = async (request: NextRequest) => {
   try {
     // Extrahování dat příspěvku z těla požadavku ve formátu JSON
     const extractPostData = await request.json();
+
+    // Server-side validace dat
+    if (!extractPostData.image) {
+      return NextResponse.json({
+        success: false,
+        message: 'Image is required.', // Zpráva pro chybějící obrázek
+      });
+    }
+
+    if (!extractPostData.title || extractPostData.title.trim().length < 3) {
+      return NextResponse.json({
+        success: false,
+        message: 'Title is required and must be at least 3 characters long.', // Zpráva pro chybějící nebo neplatný titulek
+      });
+    }
+
+    if (!extractPostData.description || extractPostData.description.trim().length < 50) {
+      return NextResponse.json({
+        success: false,
+        message: 'Description is required and must be at least 50 characters long.', // Zpráva pro chybějící nebo neplatný popis
+      });
+    }
+
+    if (!extractPostData.category) {
+      return NextResponse.json({
+        success: false,
+        message: 'Category is required.', // Zpráva pro chybějící kategorii
+      });
+    }
 
     // Vytvoření nového záznamu v databázi pomocí Prisma klienta
     const newlyCreatedPost = await prisma.post.create({
@@ -22,14 +51,12 @@ export const POST = async (request: NextRequest) => {
         success: true,
         message: 'New blog post added successfully', // Zpráva o úspěchu
       });
-      // Pokud se záznam nepodaří vytvořit, vrátí se chybová odpověď
     } else {
       return NextResponse.json({
         success: false,
         message: 'Something went wrong! Please try again', // Zpráva o neúspěchu
       });
     }
-    // Pokud dojde k chybě během zpracování požadavku, vrátí se obecná chyba
   } catch (error) {
     console.log(error); // Vypíše chybovou zprávu do konzole.
 
